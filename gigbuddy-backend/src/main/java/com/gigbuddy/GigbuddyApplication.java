ba0ec4a52ae38af4056929193bf7ce3d98bf1ba0
ashishtikhile1234 <ashishtikhile1234@users.noreply.github.com>
2026-06-11 15:36:46 +0530

    chore(backend): Spring Boot project setup with Maven and dependencies

---
+++ gigbuddy-backend/src/main/java/com/gigbuddy/GigbuddyApplication.java
@@ -0,0 +1,11 @@
+package com.gigbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigbuddyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GigbuddyApplication.class, args);
+    }
+}
